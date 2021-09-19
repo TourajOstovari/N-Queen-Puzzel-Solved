3a55f9bf47795578445c22e22770c09ef76d77c1
Toraj <Toraj@DESKTOP-I39EHRN>
2021-09-19 08:38:54 +0430

    Add project files.

---
+++ NQueen2021/IChecker.cs
@@ -0,0 +1,241 @@
+﻿namespace NQueen2021
+{
+    interface IChecker
+    {
+        bool Sotoni(byte[,] Pz); // Columns Vertical mode
+        bool Zarbdari(byte[,] Pz); // X mode 
+    }
+    public class Checker : IChecker
+    {
+        public bool Sotoni(byte[,] Pz)
+        {
+            for (int y = 0; y < Program.SizeXY; y++)
+            {
+                for (int x = 0; x < Program.SizeXY; x++)
+                {
+                    if (Pz[y, x] == 1)
+                    {
+                        if (y == 0)
+                        {
+                            { // Checks Columns Vertical Mode 1
+                                for (int i = (y + 1); i < Program.SizeXY; i++)
+                                {
+                                    if (Pz[i, x] == 1)
+                                    {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                        if (y > 0 && y < (Program.SizeXY - 1))
+                        {
+                            { // Checks Columns Vertical Mode 1
+                                for (int i = (y + 1); i < Program.SizeXY; i++)
+                                {
+                                    if (Pz[i, x] == 1)
+                                    {
+                                        return true;
+                                    }
+                                }
+                            }
+                            { // Checks Columns Vertical Mode 2
+                                for (int i = (y - 1); i > 0; i--)
+                                {
+                                    if (Pz[i, x] == 1)
+                                    {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                        if (y == (Program.SizeXY - 1))
+                        {
+                            { // Checks Columns Vertical Mode 2
+                                for (int i = (y - 1); i > 0; i--)
+                                {
+                                    if (Pz[i, x] == 1)
+                                    {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            return false;
+        }
+
+        public bool Zarbdari(byte[,] Pz)
+        {
+            for (int y = 0; y < Program.SizeXY; y++)
+            {
+                for (int x = 0; x < Program.SizeXY; x++)
+                {
+                    if (Pz[y, x] == 1)
+                    {
+                        if (y == 0)
+                        {
+
+                            { // Checks X mode    - FIXED
+                                int x2 = (x + 1);
+                                for (int y2 = (y + 1); y2 < Program.SizeXY; y2++)
+                                {
+                                    
+                                    {
+                                        if (x2 == Program.SizeXY)
+                                            break;
+                                        if (Pz[y2, x2] == 1)
+                                        {
+
+                                            return true;
+                                        }
+                                        if ((x2 + 1) >= Program.SizeXY)
+                                        {
+                                            y2 = Program.SizeXY;
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            x2++;
+                                        }
+
+                                    }
+                                }
+                            }
+                            {  // Checks X mode    -  FIXED
+                                int x2 = (x - 1);
+                                for (int y2 = (y + 1); y2 < Program.SizeXY; y2++)
+                                {
+                                    {
+                                        if (x2 < 0)
+                                        {
+                                            break;
+                                        }
+                                        if (Pz[y2, x2] == 1)
+                                        {
+                                            return true;
+                                        }
+                                        if (x2 > 0)
+                                        {
+                                            --x2;
+                                        }
+                                        if (x2 <= 0)
+                                        {
+                                            break;
+                                        }
+
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                    if (y > 0 && Pz[y, x] == 1)
+                    {
+
+
+                        { // Checks X mode    - FIXED
+                            int x2 = (x + 1);
+                            for (int y2 = (y + 1); y2 < Program.SizeXY; y2++)
+                            {
+
+                                {
+                                    if (x2 == Program.SizeXY)
+                                        break;
+                                    if (Pz[y2, x2] == 1)
+                                    {
+
+                                        return true;
+                                    }
+                                    if ((x2 + 1) >= Program.SizeXY)
+                                    {
+                                        y2 = Program.SizeXY;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        x2++;
+                                    }
+
+                                }
+                            }
+                        }
+                        {  // Checks X mode    -  FIXED
+                            int x2 = (x - 1);
+                            for (int y2 = (y + 1); y2 < Program.SizeXY; y2++)
+                            {
+                                {
+                                    if (x2 < 0)
+                                    {
+                                        break;
+                                    }
+                                    if (Pz[y2, x2] == 1)
+                                    {
+                                        return true;
+                                    }
+                                    if ((x2-1) >= 0)
+                                    {
+                                        --x2;
+                                    }
+                                    if (x2 < 0)
+                                    {
+                                        break;
+                                    }
+                                    
+                                }
+                            }
+                        }
+
+
+
+
+                        { // Checks X mode
+                            int x2 = (x - 1);
+                            for (int y2 = (y - 1); y2 >= 0; y2--)
+                            {
+
+                                {
+                                    if (x2 < 0)
+                                    {
+                                        break;
+                                    }
+                                    if (Pz[y2, x2] == 1)
+                                    { 
+                                        return true;
+                                    }
+                                }
+                                if (x2 > 0)
+                                    x2--;
+                                if ((x2-1) < 0 || (y2-1) < 0)
+                                    break;
+                            }
+                        }
+                        {  // Checks X mode
+                            int x2 = (x + 1);
+                            for (int y2 = (y - 1); y2 > 0;y2--)
+                            {
+                                
+                                {
+                                    if (x2 == Program.SizeXY)
+                                        break;
+                                    if (Pz[y2, x2] == 1)
+                                    { 
+                                        return true;
+                                    }
+                                    if (x2 < Program.SizeXY)
+                                        x2++;
+                                    if (x2 >= Program.SizeXY)
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ NQueen2021/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace NQueen2021
+{
+    using System;
+    using static System.Console;
+    class Program
+    {
+        public static int SizeXY { get; set; }
+        static byte[,] Puzzel;
+        static System.Collections.ArrayList Container = new System.Collections.ArrayList();
+        static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Blue;
+            Title = "N-Queen Problem Solver";
+            WriteLine("Hello World! It is developed by Mr. Touraj Ostovari. FOR FREE AND FUN");
+            Write("Puzzel Size? ");
+            SizeXY = int.Parse(Console.ReadLine());
+            Puzzel = new byte[SizeXY, SizeXY];
+            while (true)
+            {
+                Initialize();
+            }
+
+            //   ForegroundColor = ConsoleColor.White;
+            //   PrintPuzzel(Puzzel);
+
+
+            // ========================================================== //
+
+
+
+        }
+        static bool CheckPlus(byte[,] Pz)
+        {
+            
+            Checker ck = new Checker();
+            if (ck.Sotoni(Pz) == false && ck.Zarbdari(Pz) == false)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Found a pattern for 8-Queen");
+                PrintPuzzel(Pz);
+                Console.WriteLine("\n\tPress any key to continue ...");
+                Console.ReadKey();
+            }
+            return false;
+        }
+        static void Initialize()
+        {
+            Random rnd = new Random();
+            do
+            {
+                for (int i = 0; i < SizeXY; i++)
+                {
+                    Puzzel[i, rnd.Next(0, (SizeXY))] = 1;
+                }
+                Console.Clear();
+                //Puzzel[0, 2] = 1;
+                //Puzzel[1, 5] = 1;
+                //Puzzel[2, 7] = 1;
+                //Puzzel[3, 0] = 1;
+                //Puzzel[4, 3] = 1;
+                //Puzzel[5, 6] = 1;
+                //Puzzel[6, 4] = 1;
+                //Puzzel[7, 1] = 1;
+                PrintPuzzel(Puzzel);
+                if (!Container.Contains(Puzzel))
+                {
+                    Container.Add(Puzzel);
+                    CheckPlus(Puzzel);
+                }
+                Puzzel = new byte[SizeXY, SizeXY];
+                System.Threading.Thread.Sleep(5);
+            } while (true);
+        }
+        static void PrintPuzzel(byte[,] Pz)
+        {
+            for (int i1 = 0; i1 < SizeXY; i1++)
+            {
+                for (int i2 = 0; i2 < SizeXY; i2++)
+                {
+                    if (Pz[i1, i2] == 0)
+                        Write("0\t");
+                    else
+                        Write("1\t");
+                }
+                Write("\n");
+            }
+        }
+    }
+}
